3e27edd58721806821a9efa0e7f7eb0ed8105b42
diendaica <diennv@cnv.vn>
2026-04-03 18:04:08 +0700

    Function homework

---
+++ funct.js
@@ -0,0 +1,35 @@
+function isPasswordValid(password) { //hàm kiểm tra độ dài mật khẩu
+    if (password.length >= 8) {
+        console.log("Mật khẩu hợp lệ.");
+        return true;
+    } else {
+        console.log("Mật khẩu không hợp lệ. Nó phải có ít nhất 8 ký tự.");
+        return false;
+    }
+}
+
+isPasswordValid("1234aD@a"); //nhập mật khẩu để kiểm tra
+
+console.log("-----------------------------");
+function formatPrice(amount) {
+    // return amount + "$"
+    return `${amount}$`
+}
+
+console.log(formatPrice(50000)); //nhập số tiền để định dạng
+
+console.log("-----------------------------");
+
+const myBugs = [
+    { id: 1, severity: "High" },
+    { id: 2, severity: "Low" },
+    { id: 3, severity: "High" }
+];
+
+function getHighSeverityBugs(list) {
+    return list.filter(function (bug) {
+        return bug.severity === "Low";
+    });
+}
+
+console.log(getHighSeverityBugs(myBugs));
