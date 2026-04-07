1e1f1efcdfecfd8538553617f6642364c0eb79b9
diendaica <diennv@cnv.vn>
2026-04-07 16:51:41 +0700

    update lesson 3 of asynchonus

--- asynchonus.js
+++ asynchonus.js
@@ -56,7 +56,7 @@
 
 
 console.log("-----------Bài tập 2------------");
-// Bài tập 2: "Bẫy lỗi" hệ thống (Error Handling)
+// Bài tập 2: "Bẫy lỗi" hệ thống(Error Handling)
 // Đề bài: Trong Automation, đôi khi Server sẽ trả về lỗi (ví dụ: Timeout 404). 
 // Bạn cần viết một script "nồi đồng cối đá" không bị crash khi gặp lỗi.
 // Yêu cầu: 
@@ -91,19 +91,49 @@ console.log("-----------Bài tập 2------------");
 
 // runTest();
 
+console.log("-----------Question 6------------");
+// console.log('A');
 
-console.log('A');
+console.log("----Bài tập 3-----");
+// Bài tập 3: Kịch bản Test song song (Parallel vs Sequential)
+// Đề bài: Giả sử bạn cần kiểm tra 3 Page khác nhau: HomePage, ProductPage, và ContactPage. Mỗi trang mất 2 giây để load.
+// Yêu cầu:
+// 1. Viết một hàm checkPage(pageName) trả về Promise và hoàn thành sau 2 giây.
+// 2. Kịch bản A (Tuần tự): Dùng await để kiểm tra từng trang một (Trang 1 xong mới đến trang 2).
+//  Sau đó in ra tổng thời gian chạy (dự kiến là 6 giây).
+// 3. Kịch bản B (Song song - Nâng cao): Tìm hiểu về Promise.all() để kích hoạt kiểm tra cả 3 trang
+//  cùng một lúc. Sau đó in ra tổng thời gian chạy (dự kiến chỉ mất 2 giây).
 
-setTimeout(() => console.log('B'), 0);
-
-Promise.resolve().then(() => console.log('C'));
-
-async function quickTask() {
-    console.log('D');
-    await Promise.resolve();
-    console.log('E');
+// Câu 1
+const checkPage = (pageName) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Đang kiểm tra trang: " + pageName);
+            resolve();
+        }, 2000);
+    });
 }
 
-quickTask();
+// //Câu 2
+// async function testSequential() {
+//     const start = Date.now();
+//     await checkPage("HomePage");
+//     await checkPage("ProductPage");
+//     await checkPage("ContactPage");
+//     const end = Date.now();
+//     console.log("Tổng thời gian chạy (Tuần tự): " + (end - start) / 1000 + " giây");
+// }
+// testSequential();
 
-console.log('F');
+//Câu 3
+async function testParallel() {
+    const start = Date.now();
+    const result = await Promise.all([
+        checkPage("HomePage"),
+        checkPage("ProductPage"),
+        checkPage("ContactPage")
+    ]);
+    const end = Date.now();
+    console.log("Tổng thời gian chạy (Song song): " + (end - start) / 1000 + " giây");
+}
+testParallel();
